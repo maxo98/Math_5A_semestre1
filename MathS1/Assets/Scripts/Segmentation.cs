4fba3890e6a6b76f36aa43011f89854c356fe913
Mickaël BERTHELOT <41340047+crread@users.noreply.github.com>
2023-02-14 18:23:11 +0100

    update c++ api and added in c#
    I updated c++ api wit function to set data from c# part to c++ part. these functions are implemented in c# script and seem to work fine.

--- MathS1/Assets/Scripts/Segmentation.cs
+++ MathS1/Assets/Scripts/Segmentation.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Collections;
 using System;
+using System.Numerics;
 using System.Runtime.InteropServices;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class Segmentation : MonoBehaviour
 {
@@ -15,12 +18,16 @@ public class Segmentation : MonoBehaviour
 
     private IntPtr _hyperneatInstance;
     private IntPtr _hyperneatParamsInstance;
-    private IntPtr _neatParamsInstance;
+    private IntPtr _dataSetInstance;
     
+    private List<Tuple<Vector3, List<bool>>> _bones;
+
+    private IntPtr _neatParamsInstance;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("started");
+        _bones = new List<Tuple<Vector3, List<bool>>>();
         
         List<List<int>> bonesVertices = new List<List<int>>();
         
@@ -35,7 +42,7 @@ public class Segmentation : MonoBehaviour
         // Vector3[] vert = tmp.vertices;
 
         Vector3[] vert = meshFilter.mesh.vertices;
-
+        
         // Get the number of bone weights per vertex
         NativeArray<byte> bonesPerVertex = mesh.GetBonesPerVertex();
         if (bonesPerVertex.Length == 0)
@@ -52,12 +59,20 @@ public class Segmentation : MonoBehaviour
         // Iterate over the vertices
         for (int vertIndex = 0; vertIndex < mesh.vertexCount; vertIndex++)
         {
+            var newList = new List<bool>(skinMesh.bones.Length);
+
+            for (int i = 0; i < skinMesh.bones.Length; i++)
+            {
+                newList.Add(false);
+            }
+
             int numberOfBonesForThisVertex = bonesPerVertex[vertIndex];
 
             // For each vertex, iterate over its BoneWeights
             for (int i = 0; i < numberOfBonesForThisVertex; i++)
             {
                 int index = boneWeights[boneWeightIndex].boneIndex;
+                newList[index] = true;
                 
                 if(boneWeights[boneWeightIndex].weight > 0.05)
                 {
@@ -66,6 +81,9 @@ public class Segmentation : MonoBehaviour
 
                 boneWeightIndex++;
             }
+            
+            var newTuple = Tuple.Create(mesh.vertices[vertIndex], newList);
+            _bones.Add(newTuple);
         }
 
         Debug.Log(bonesVertices[0].Count);
@@ -80,6 +98,21 @@ public class Segmentation : MonoBehaviour
         _hyperneatParamsInstance = CreateHyperNeatParamInstance();
         _hyperneatInstance =
             CreateHyperNeatInstance(bonesVertices.Count, _neatParamsInstance, _hyperneatParamsInstance);
+
+        _dataSetInstance = InitDataSet(skinMesh.bones.Length);
+        
+        for (int idx = 0; idx < _bones.Count; idx++)
+        {
+            var pair = _bones[idx];
+            float[] positionConvertedToArray = new float[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                positionConvertedToArray[i] = pair.Item1[i];
+            }
+
+            SetNewVertex(_dataSetInstance, positionConvertedToArray, 3, pair.Item2.ToArray(), pair.Item2.Count);
+        }
     }
 
     // Update is called once per frame
@@ -93,13 +126,23 @@ public class Segmentation : MonoBehaviour
         DeleteInstance(_hyperneatInstance);
         DeleteInstance(_hyperneatParamsInstance);
         DeleteInstance(_neatParamsInstance);
+        DeleteInstance(_dataSetInstance);
     }
 
     [DllImport("machine learning algo")]
     static extern void ApplyBackProp(IntPtr hyperneatInstance);
+
+    [DllImport("machine learning algo")]
+    static extern IntPtr InitDataSet(int boneSize);
     
     [DllImport("machine learning algo")]
+    static extern void SetNewVertex(IntPtr dataset, float[] position, int lengthPosition, bool[] linkedBones, int lengthLink);
+
+    [DllImport("machine learning algo")]
     static extern void DeleteInstance(IntPtr instance);
+ 
+    [DllImport("machine learning algo")]
+    static extern void DeleteArrayInstance(IntPtr arrayInstance);
     
     [DllImport("machine learning algo")]
     static extern IntPtr CreateHyperNeatInstance(int popSize, IntPtr instanceNeatParams, IntPtr instanceHyperneatParams);
--- machine learning algo/dllmain.cpp
+++ machine learning algo/dllmain.cpp
@@ -3,13 +3,61 @@
 #include "Neat.h"
 #include "Hyperneat.h"
 
+#include <vector>
+#include <map>
+
+#define DLL_EXPORT __declspec(dllexport)
+
+template<typename type>
+std::vector<type> wrapperArrayToVector(const type* data, const size_t length)
+{
+	std::vector<type> newVector = std::vector<type>(length);
+
+	for (int i = 0; i < length; i++)
+	{
+		newVector[i] = data[i];
+	}
+
+	return newVector;
+}
+
+struct DataSet {
+	int bones;
+	std::vector<std::pair<std::vector<float>, std::vector<bool>>> vertices;
+};
+
 extern "C"
 {
-	NeatParameters* CreateNeatParamInstance();
-	HyperneatParameters* CreateHyperNeatParamInstance();
-	Hyperneat* CreateHyperNeatInstance(unsigned int popSize, NeatParameters* neatParams, HyperneatParameters* hyperneatParams);
-	void ApplyBackProp(Hyperneat* instance);
-	void DeleteInstance(void* instance);
+	DLL_EXPORT NeatParameters* CreateNeatParamInstance();
+	DLL_EXPORT HyperneatParameters* CreateHyperNeatParamInstance();
+	DLL_EXPORT Hyperneat* CreateHyperNeatInstance(unsigned int popSize, NeatParameters* neatParams, HyperneatParameters* hyperneatParams);
+	DLL_EXPORT DataSet* InitDataSet(int numberBones);
+	DLL_EXPORT void SetNewVertex(DataSet* dataset, float* position, int length, bool* linkedBones, int lengthLink);
+	DLL_EXPORT void ApplyBackProp(Hyperneat* instance);
+	DLL_EXPORT void DeleteInstance(void* instance);
+	DLL_EXPORT void DeleteArrayInstance(void* instance);
+}
+
+void SetNewVertex(DataSet* dataset, float* position, int length, bool* linkedBones, int lengthLink)
+{
+	std::vector<float> lPosition = wrapperArrayToVector<float>(position, length);
+	std::vector<bool> lLinkedBones = wrapperArrayToVector<bool>(linkedBones, lengthLink);
+
+	std::pair<std::vector<float>, std::vector<bool>> newPair = std::make_pair(lPosition, lLinkedBones);
+	dataset->vertices.push_back(newPair);
+}
+
+void SetBones(DataSet* dataset, int sizeBones)
+{
+	dataset->bones = sizeBones;
+}
+
+DataSet* InitDataSet(int numberBones)
+{
+	DataSet* newDataSet = new DataSet();
+	newDataSet->bones = numberBones;
+
+	return newDataSet;
 }
 
 void ApplyBackProp(Hyperneat* instance)
@@ -39,4 +87,9 @@ NeatParameters* CreateNeatParamInstance()
 void DeleteInstance(void* instance)
 {
 	delete instance;
+}
+
+void DeleteArrayInstance(void* instance)
+{
+	delete[] instance;
 }
